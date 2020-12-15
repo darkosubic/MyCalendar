60a160d67e665fff7dc01dc69e86b9e372ad271d
Darko Subić <darko.subic1@gmail.com>
2020-12-16 00:22:05 +0100

    [MCL-1] Created initial models of the database and created UML diagram as Visio file.

---
+++ MyCalendar.Data/Models/DailyEventType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyCalendar.Data.Models
+{
+    public record DailyEventType
+    {
+        public DailyEventType(Guid id, string defaultName) 
+            => (Id, DefaultName) = (id, defaultName);
+        
+        public Guid Id { get; }
+        
+        public string DefaultName { get; }
+    }
+}
